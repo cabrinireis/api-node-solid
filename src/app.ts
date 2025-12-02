a4b302f8e79af0efc022681ac0ebf84df12f9110
Cabrini <cabrinireis@gmail.com>
2025-12-02 15:33:22 +0000

    feat: controller register

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,5 @@
 import fastify from 'fastify'
-import { prisma } from './lib/prisma'
-import { z } from 'zod'
+import { appRoutes } from '@/http/routes'
 export const app = fastify()
 
-app.post('/users', async (request, reply) => {
-    const createUserSchema = z.object({
-        name: z.string(),
-        email: z.string(),
-        password: z.string().min(6),
-    })
-
-    const { name, email, password } = createUserSchema.parse(request.body)
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password,
-        },
-    })
-
-    return reply.status(201).send({ message: 'User created successfully' })
-})
+app.register(appRoutes)
---
+++ src/http/controller/register.ts
@@ -0,0 +1,23 @@
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+export const app = fastify()
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+    const createUserSchema = z.object({
+        name: z.string(),
+        email: z.string(),
+        password: z.string().min(6),
+    })
+
+    const { name, email, password } = createUserSchema.parse(request.body)
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password,
+        },
+    })
+
+    return reply.status(201).send({ message: 'User created successfully' })
+}
