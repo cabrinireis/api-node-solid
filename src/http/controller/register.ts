51ccac7f05034cf02668de3a6f7188bf27f09770
Cabrini <cabrinireis@gmail.com>
2025-12-02 15:58:47 +0000

    feat: create use case register

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -2,6 +2,7 @@ import fastify, { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
 import { z } from 'zod'
+import { registerUseCase } from '@/use-cases/register'
 export const app = fastify()
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -12,25 +13,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     })
 
     const { name, email, password } = createUserSchema.parse(request.body)
-    const password_hash = await hash(password, 5)
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email,
-        },
-    })
-
-    if (userWithSameEmail) {
-        return reply.status(409).send({ message: 'User already exists' })
+    try {
+        await registerUseCase({ name, email, password })
+    } catch (error) {
+        return reply.status(400).send({ message: (error as Error).message })
     }
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash,
-        },
-    })
-
     return reply.status(201).send({ message: 'User created successfully' })
 }
