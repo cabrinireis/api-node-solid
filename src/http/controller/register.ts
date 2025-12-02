1230f5fcdd790720a15420bfdc01481ffa387703
Cabrini <cabrinireis@gmail.com>
2025-12-02 15:40:51 +0000

    feat: hash of password and validation email address

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -1,5 +1,6 @@
 import fastify, { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { z } from 'zod'
 export const app = fastify()
 
@@ -11,11 +12,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     })
 
     const { name, email, password } = createUserSchema.parse(request.body)
+    const password_hash = await hash(password, 5)
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    })
+
+    if (userWithSameEmail) {
+        return reply.status(409).send({ message: 'User already exists' })
+    }
+
     await prisma.user.create({
         data: {
             name,
             email,
-            password_hash: password,
+            password_hash,
         },
     })
 
