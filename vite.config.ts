de3fae23d882d89c5ef344faf9ac64bec9342fa1
cabrini <cabrinireis@gmail.com>
2026-05-06 12:07:35 +0100

    chore: fix warning vit config about tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config'
-import tsconfigPatchs from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [tsconfigPatchs()],
+    resolve: {
+        tsconfigPaths: true
+    }
 })
