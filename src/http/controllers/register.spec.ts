c4ffc4dffd980c12a5748b822445ecb6088f4ef6
cabrini <cabrinireis@gmail.com>
2026-06-02 16:30:36 +0100

    feat: enhance testing setup with Vitest configuration and Prisma environment

---
+++ src/http/controllers/register.spec.ts
@@ -0,0 +1,9 @@
+// crie um teste simples
+import { expect, describe, it } from 'vitest'
+
+describe('Example Test', () => {
+    it('should be true', () => {
+        expect(true).toBe(true)
+        
+    })
+})
