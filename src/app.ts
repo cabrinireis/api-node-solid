b58619c522f76937218249a388d3b18ab6c5b035
Cabrini <cabrinireis@gmail.com>
2026-06-17 15:58:07 +0100

    feat: implement refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { usersRoutes } from '@/http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
@@ -8,15 +9,22 @@ import { ZodError } from 'zod'
 export const app = fastify()
 import { env } from '@/env'
 
-
-
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false,
+    },
+    sign: {
+        expiresIn: '10m',
+    },
 })
 
 app.addHook('onClose', async () => {
     await prisma.$disconnect()
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -22,14 +22,34 @@ export async function authenticate(
             password,
         })
 
-        const token = await reply.jwtSign({}, {
-            sign: {
-                sub: user.id,
+        const token = await reply.jwtSign(
+            {},
+            {
+                sign: {
+                    sub: user.id,
+                },
             }
-        })
+        )
 
-        return reply.status(200).send({ token })
+        const refreshToken = await reply.jwtSign(
+            {},
+            {
+                sign: {
+                    sub: user.id,
+                    expiresIn: '7d',
+                },
+            }
+        )
 
+        return reply
+            .status(200)
+            .setCookie('refreshToken', refreshToken, {
+                path: '/',
+                secure: true,
+                sameSite: true,
+                httpOnly: true,
+            })
+            .send({ token })
     } catch (err) {
         if (err instanceof InvalidCredentialsError) {
             return reply.status(400).send({ message: err.message })
@@ -37,5 +57,4 @@ export async function authenticate(
 
         throw err
     }
-
 }
