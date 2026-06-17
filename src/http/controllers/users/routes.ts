f43647582ab30c7050fa3a9f552ab1a07ff493e8
Cabrini <cabrinireis@gmail.com>
2026-06-17 16:42:08 +0100

    feat: implement RBAC roles user

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -9,6 +9,6 @@ export async function usersRoutes(app: FastifyInstance) {
     app.post('/users', register)
     app.post('/sessions', authenticate)
 
-    app.patch('token/refresh', refresh)
+    app.patch('/token/refresh', refresh)
     app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
